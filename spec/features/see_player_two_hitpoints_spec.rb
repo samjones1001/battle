7c10016957c9a501f8f6b2272bf71785635daf0e
Sam Jones <sam.jones494@googlemail.com>
2017-01-19 08:21:52 +0000

    shows player two's hit points

--- spec/features/see_player_two_hitpoints_spec.rb
+++ spec/features/see_player_two_hitpoints_spec.rb
@@ -1,3 +1,17 @@
 #As Player 1,
-So I can see how close I am to winning
-I want to see Player 2's Hit Points
+#So I can see how close I am to winning
+#I want to see Player 2's Hit Points
+
+feature 'View hitpoints' do
+  
+  scenario "viewing player two's hitpoints" do
+   
+    visit('/')
+    fill_in :player_one_name, with: 'Player 1'
+    fill_in :player_two_name, with: 'Player 2'
+    click_button 'Fight!'
+    expect(page).to have_content 'Player 2 has 100 hit points'
+  
+  end
+
+end
